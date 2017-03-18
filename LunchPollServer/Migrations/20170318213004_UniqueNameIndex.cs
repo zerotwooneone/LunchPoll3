198395d6121613a0ecf4c4cfbd0a23509457a0f4
David Squire <dsquire@travisoft.com>
2017-03-18 17:36:03 -0400

    Prevent duplicate nomination names

---
+++ LunchPollServer/Migrations/20170318213004_UniqueNameIndex.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LunchPollServer.Migrations
+{
+    public partial class UniqueNameIndex : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Nominations_Name",
+                table: "Nominations",
+                column: "Name",unique:true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(name: "IX_Nominations_Name");
+        }
+    }
+}
