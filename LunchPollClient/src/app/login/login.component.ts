e9f7b470c73faa63974d3d16bc8fa09e3bfc39ef
David Squire <dsquire@travisoft.com>
2017-04-05 00:34:28 -0400

    added a hack so I can test this when compiling in prod mode

--- LunchPollClient/src/app/login/login.component.ts
+++ LunchPollClient/src/app/login/login.component.ts
@@ -9,7 +9,7 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-    constructor(private auth: Auth,
+    constructor(public auth: Auth,
         private router: Router) { }
 
     ngOnInit() {
