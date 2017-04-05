24549845f70e3ac4321c0a3c473505484d9e5aae
David Squire <dsquire@travisoft.com>
2017-04-05 01:13:59 -0400

    set the server to use a specific port

--- LunchPollServer/Program.cs
+++ LunchPollServer/Program.cs
@@ -13,6 +13,7 @@ namespace LunchPollServer
         public static void Main(string[] args)
         {
             var host = new WebHostBuilder()
+                .UseUrls("http://0.0.0.0:4200")
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
