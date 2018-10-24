acd5785c299d2fd7a6aa16e185692a243c2402c9
David Squire <squiredd@gmail.com>
2018-10-24 16:36:56 -0700

    regenerated the client app with scss and native-view-encapsulation and ZH prefix

--- LunchPollServer/ClientApp/src/app/app.component.ts
+++ LunchPollServer/ClientApp/src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'zh-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'LunchPollApp';
+  title = 'ClientApp';
 }
