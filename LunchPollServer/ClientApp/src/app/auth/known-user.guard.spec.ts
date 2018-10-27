bade4ac1dcea31289e7df7501f0727ec0f3cda1e
David Squire <squiredd@gmail.com>
2018-10-27 11:48:49 -0700

    fixed auth guard spec issue

--- LunchPollServer/ClientApp/src/app/auth/known-user.guard.spec.ts
+++ LunchPollServer/ClientApp/src/app/auth/known-user.guard.spec.ts
@@ -1,11 +1,20 @@
 import { TestBed, async, inject } from '@angular/core/testing';
 
 import { KnownUserGuard } from './known-user.guard';
+import { UserIdSource } from '../user-id/user-id-source';
+import { UserIdRepositoryService } from '../user-id/user-id-repository.service';
+import { BehaviorSubject } from 'rxjs';
+import { UserIdModel } from '../user-id/user-id.model';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('KnownUserGuard', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [KnownUserGuard]
+      providers: [
+        KnownUserGuard,
+        {provide: UserIdSource, useFactory: () => new UserIdRepositoryService(new BehaviorSubject<UserIdModel>(null)) }
+      ],
+      imports: [RouterTestingModule]
     });
   });
 
